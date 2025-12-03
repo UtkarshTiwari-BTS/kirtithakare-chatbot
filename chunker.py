34713a300d0bfdbb664d3289eb0e2fa95353500a
UtkarshTiwari-BTS <utkarsh.tiwari@bajajtechnologyservices.com>
2025-12-04 00:46:17 +0530

    Update chunker.py

--- chunker.py
+++ chunker.py
@@ -1,12 +1,9 @@
-def chunk_text(text, chunk_size=400):
+def chunk_text(text, chunk_size=1000):
     words = text.split()
-    #Splits the text into words on whitespace — creates a list of words.
-    #(list comprehension)
-    #chunks by slicing the words list every chunk_size words. 
-    # For each slice, it rejoins words with spaces into a chunk string.
-    chunks = [
-        " ".join(words[i:i + chunk_size])
-        for i in range(0, len(words), chunk_size)
-    ]
-    return chunks
+    chunks = []
 
+    for i in range(0, len(words), chunk_size):
+        chunk = " ".join(words[i:i + chunk_size])
+        chunks.append(chunk)
+
+    return chunks
